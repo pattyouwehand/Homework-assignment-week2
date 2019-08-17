3e7ad602fb60b9dcf83cbd616045a34d78df6db9
pattyouwehand <pattyouwehand@gmail.com>
2019-08-17 19:17:03 +0200

    final

--- src/async-functions.js
+++ src/async-functions.js
@@ -4,26 +4,11 @@ function getTatooineResidents(){
 
   const promise = fetch("https://swapi.co/api/planets/1/")
   promise
-  .then(response => response.json()) // parameter geeft volgende regel door (json)
-  .then(data => fetch(data.residents))
-  .then(res => console.log(res.url))
+  .then(response => response.json())
+  .then(data => console.log(data.residents))
   .catch(err => console.log(err))
-
-  /*The function should a return promise which resolves with an array of urls for the residents of Tatooine like : 
-  [
-    ${planets1.residents.map(url => `\n              "${url}"`)} \n
-  ] 
-the promise that was returned from you function resolved with: ${residents}`
 }
 
-const promise = fetch("https://swapi.co/api/planets/1/")
-  promise
-  .then(response => response.json()) // parameter geeft volgende regel door (json)
-  .then(data => fetch(data.residents))
-  .then(res => console.log(res.url))
-  .catch(err => console.log(err))
-
-*/
 function promiseMeAString(input) {
     return new Promise((resolve, reject) => {
       if (!input) {
@@ -32,16 +17,6 @@ function promiseMeAString(input) {
         resolve('You kept the Promise!')
     })
   }
-
-
-
-
-
-
-
-
-
-
 
 module.exports = {
     getTatooineResidents,
