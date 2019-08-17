f4cad487c25d0c5d9d87d288c5ebd54d18ea3aa5
pattyouwehand <pattyouwehand@gmail.com>
2019-08-17 09:13:30 +0200

    return promise

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,17 +1,29 @@
 const fetch = require('node-fetch')
 
 function getTatooineResidents(){
-    
-    fetch("https://swapi.co/api/planets/1/")
-      .then(response => response.json()) // parameter geeft volgende regel door (json)
-      .then(data => fetch(data.residents))
-      .then(res => console.log(res)) //aanpassen
-      //.then(homeworld => homeworld.json())//aanpassen
-      //.then(item => console.log(item.name))
-      .catch(err => console.log(err))
-}    
 
+  const promise = fetch("https://swapi.co/api/planets/1/")
+  promise
+  .then(response => response.json()) // parameter geeft volgende regel door (json)
+  .then(data => fetch(data.residents))
+  .then(res => console.log(res.url))
+  .catch(err => console.log(err))
 
+  /*The function should a return promise which resolves with an array of urls for the residents of Tatooine like : 
+  [
+    ${planets1.residents.map(url => `\n              "${url}"`)} \n
+  ] 
+the promise that was returned from you function resolved with: ${residents}`
+}
+
+const promise = fetch("https://swapi.co/api/planets/1/")
+  promise
+  .then(response => response.json()) // parameter geeft volgende regel door (json)
+  .then(data => fetch(data.residents))
+  .then(res => console.log(res.url))
+  .catch(err => console.log(err))
+
+*/
 function promiseMeAString(input) {
     return new Promise((resolve, reject) => {
       if (!input) {
